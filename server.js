5945d91b57ca67c2a099c7f36a850ad592a39fba
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-04-04 18:46:13 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const express = require("express");
+const cors = require("cors");
+const superagent = require("superagent");
+const request = require("request");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3002;
+
+// ==========================================================================
+
+
+// Routes
+app.get("/", function (req, res) {
+  res.send("hi!");
+});
+
+// app.get("/dateIdeas",dateHandler)
+
+// app.get("/user",User.getSomething)
+// app.post("/user",User.createSomething)
+// app.delete("/user/:index",User.deleteSomething)
+// app.put("/user/:index",User.updateSomething)
+
+// STUFF NEEDED
+// get data from api (need to query location) => refer to city explorer
+
+// Create CRUD adding data into user account => refer to best books
+
+
+// ===========================================================================
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
